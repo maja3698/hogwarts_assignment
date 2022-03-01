d0bef1bd37d3922c07b37a87cab020d44fcc481f
melairm <mela3411@stud.kea.dk>
2022-03-01 19:45:56 +0100

    searchbar-works

--- hogwarts.js
+++ hogwarts.js
@@ -128,12 +128,15 @@ function setUp() {
     .querySelectorAll("[data-action='filterS']")
     .forEach((button) => button.addEventListener("click", selectFilterS));
 
-    document
-    .querySelector("[data-action='filterP']").addEventListener("click", selectFilterP);
+  document
+    .querySelector("[data-action='filterP']")
+    .addEventListener("click", selectFilterP);
 
-    document
-    .querySelector("[data-action='filterI']").addEventListener("click", selectFilterI);
+  document
+    .querySelector("[data-action='filterI']")
+    .addEventListener("click", selectFilterI);
 
+  document.querySelector(".searchbar").addEventListener("input", searchBar);
 
   // SORTING EVENTS:
   document
@@ -143,6 +146,18 @@ function setUp() {
     .forEach((button) => button.addEventListener("click", selectSort));
 
   loadJSON();
+}
+
+function searchBar(e) {
+  const searchString = e.target.value.toLowerCase();
+  const searchedStudents = allStudents.filter((student) => {
+    return (
+      student.firstname.toLowerCase().includes(searchString) ||
+      student.lastname.toLowerCase().includes(searchString) ||
+      student.house.toLowerCase().includes(searchString)
+    );
+  });
+  displayList(searchedStudents);
 }
 
 async function loadJSON() {
@@ -271,11 +286,10 @@ function selectFilterS(event) {
   filterSList(filter);
 }
 
-function selectFilterP (event) {
+function selectFilterP(event) {
   const filter = event.target.dataset.filter;
   console.log(`user select, ${filter}`);
   filterPList(filter);
-
 }
 
 function selectFilterI(event) {
@@ -287,9 +301,7 @@ function selectFilterI(event) {
 function filterIList(filter) {
   filterStudents = allStudents;
   if (filter === "squad") {
-    filterStudents = filterStudents.filter(
-      (student) => student.squad === true
-    );
+    filterStudents = filterStudents.filter((student) => student.squad === true);
   } else {
     filterStudents = filterStudents.filter(
       (student) => student.squad === false
@@ -487,11 +499,12 @@ function displayStudent(student) {
 
     if (student.squad) {
       // Member of the Inquisitory Squad
-      document.querySelector("#popup-sq").textContent = "Member of the Inquisitory Squad";
+      document.querySelector("#popup-sq").textContent =
+        "Member of the Inquisitory Squad";
     } else {
-      document.querySelector("#popup-sq").textContent = "Not Member of the Inquisitory Squad";
+      document.querySelector("#popup-sq").textContent =
+        "Not Member of the Inquisitory Squad";
     }
-
 
     //FOR HOUSE IMG
 
@@ -500,12 +513,9 @@ function displayStudent(student) {
     // ).src = `/assets/${student.house}-flag.svg`;
     // document.querySelector("#house-logo").src = `/assets/${student.house}.png`;
 
-  
     document.querySelector(
       "#house-logo"
     ).src = `/house-flags/${student.house}.svg`;
-    
-
 
     document.querySelector("#popup-house").textContent = student.house;
 
@@ -526,8 +536,9 @@ function displayStudent(student) {
       }_${student.firstname.charAt(0)}.png`;
     }
 
-    document.querySelector("#house-logo").src = `/house-flags/${student.house}.svg`;
-
+    document.querySelector(
+      "#house-logo"
+    ).src = `/house-flags/${student.house}.svg`;
 
     // document.querySelector("#house-logo").src = `/icons/${student.house}.png`;
 
@@ -557,30 +568,27 @@ function displayStudent(student) {
       console.log(student.firstname + " is expelled");
       buildList();
     }
-  
 
-  //     <div id="popup-header">
-  //     <div id="popup-title">
-  //         <h2 id="popup-name">Harry Potter</h2>
-  //         <h3 id="popup-house">House <span id="stud-house">Griffindor</span></h3>
-  //     </div>
-  //     <div id="popup-house-logo">G</div>
-  // </div>
-  // <p id="popup-pref">Prefect</p>
-  // <p id="popup-p-blood">Pure Blood</p>
-  // <p id="popup-h-blood">Half Blood</p>
-  // <p id="popup-muggle">Muggle</p>
-  // <p id="popup-sq"><span id="popup-squad">Not </span>Member of the Inquisitory Squad</p>
-  // <p id="popup-stud-st">Student Status: <span id="poup-status">Regular</span></p>
-  // <div id="popup-btns">
-  //     <button id="popup-expell">Expell Student</button>
-  //     <button id="popup-close">Close Window</button>
-  // </div>
-
+    //     <div id="popup-header">
+    //     <div id="popup-title">
+    //         <h2 id="popup-name">Harry Potter</h2>
+    //         <h3 id="popup-house">House <span id="stud-house">Griffindor</span></h3>
+    //     </div>
+    //     <div id="popup-house-logo">G</div>
+    // </div>
+    // <p id="popup-pref">Prefect</p>
+    // <p id="popup-p-blood">Pure Blood</p>
+    // <p id="popup-h-blood">Half Blood</p>
+    // <p id="popup-muggle">Muggle</p>
+    // <p id="popup-sq"><span id="popup-squad">Not </span>Member of the Inquisitory Squad</p>
+    // <p id="popup-stud-st">Student Status: <span id="poup-status">Regular</span></p>
+    // <div id="popup-btns">
+    //     <button id="popup-expell">Expell Student</button>
+    //     <button id="popup-close">Close Window</button>
+    // </div>
   }
   document.querySelector("#list tbody").appendChild(clone);
 }
-
 
 function closePU() {
   document.querySelector("#student-popup").classList.add("hidden");
