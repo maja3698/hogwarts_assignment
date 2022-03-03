89a33ba012eabfc560067672a2b8130aa09fd834
Maja Berendsen <maja498h@stud.kea.dk>
2022-03-03 17:32:22 +0100

    fixed-Squad

--- hogwarts.js
+++ hogwarts.js
@@ -435,9 +435,9 @@ function displayStudent(student) {
   }
 
   function squadClicked() {
-    document.querySelector("#squad-btn").addEventListener("click", closeSquad);
+    // document.querySelector("#squad-btn").addEventListener("click", closeSquad);
 
-    document.querySelector("#squad-popup").classList.remove("hidden");
+    // document.querySelector("#squad-popup").classList.remove("hidden");
     console.log("squad is clicked");
 
     if (systemHacked === false) {
